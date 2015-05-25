40225f2055048cef6eea740359676463012997bb
Jesse Talavera-Greenberg <jessetalavera@aol.com>
2015-05-25 00:37:04 -0400

    Added the asset list

---
+++ Assets/Data/PlanetAssetData.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlanetAssetData : ScriptableObject
+{
+    [Tooltip("Spheres of different fidelity")]
+    public Mesh
+        PlanetMesh;
+    [Tooltip("The planet that will be created")]
+    public GameObject
+        PlanetPrefab;
+    [Tooltip("The prefab used for explosions")]
+    public GameObject
+        ExplosionPrefab;
+    [Tooltip("The materials a planet can be rendered with.  Randomly chosen on creation")]
+    public Material[]
+        Materials;
+}
